e965317ef1768b232f0cad47bc4ecd3f8e1a31ff
Leon Helwerda <l.s.helwerda@umail.leidenuniv.nl>
2014-11-25 22:38:22 +0100

    Read more statistics
    - Compression statistics
    - Speed tests
    - Sizes

--- filter.py
+++ filter.py
@@ -2,6 +2,7 @@
 import sys
 from glob import glob
 import re
+import os
 
 class Dataset:
     def __init__(self, name):
@@ -37,6 +38,7 @@ def main(argv):
     properties = glob(path + "/*.properties")
     for p in properties:
         experiment = p[len(path):-len(".properties")]
+        prefix = path + "/" + experiment
 
         m = re.match(regex, experiment)
         print(m.groups())
@@ -46,7 +48,7 @@ def main(argv):
             sets[name] = Dataset(name)
 
         if compression not in sets[name].compressions:
-            sets[name].compressions[compression] = []
+            sets[name].compressions[compression] = {}
 
         data = Compression(compression, params)
 
@@ -57,13 +59,63 @@ def main(argv):
 
             prop, value = line[:-1].split("=")
             if prop in property_filter:
-                print(prop, value)
                 data.metrics[prop] = value
 
         prop_file.close()
 
-        sets[name].compressions[compression].append(data)
+        stats_file = open(prefix + ".stats")
+        for line in stats_file:
+            if " - " in line:
+                info = line[:-1].split(" - ")[1]
+                if info == "Loading graph...":
+                    key = "loadtime"
+                elif info == "Storing...":
+                    key = "storetime"
+                elif info.startswith("Elapsed: "):
+                    time = info[len("Elapsed: "):info.find(" [")]
+                    data.metrics[key] = time
+            elif line.startswith("mem"):
+                value = line[len("mem     "):-len("/4 kB\n")]
+                data.metrics["peakmem"] = int(value)/4
 
+        stats_file.close()
+
+        for speed in ["seq", "rand"]:
+            speed_file = open(prefix + "." + speed + "speedtest")
+            for line in speed_file:
+                if line.startswith("Time: "):
+                    parts = line.split(" ")
+                    for p in range(0, len(parts), 2):
+                        key = parts[p].lower()[:-1]
+                        value = parts[p+1]
+                        if not value[-1].isdigit():
+                            value = value[:-1]
+                        data.metrics[speed + "_" + key] = value
+            speed_file.close()
+
+        try:
+            st = os.stat(prefix + ".graph")
+            data.metrics["size"] = st.st_size
+        except OSError:
+            data.metrics["size"] = None
+
+        print(data.metrics)
+        sets[name].compressions[compression][params] = data
+
+    sizes = glob(path + "/*.sizes")
+    for s in sizes:
+        size_file = open(s)
+        for line in size_file:
+            parts = line[:-1].split()
+            size = int(parts[5])
+            experiment = parts[-1]
+            if experiment.endswith(".graph"):
+                m = re.match(regex, experiment[:-len(".graph")])
+                print(m.groups(), size)
+                name, compression, params = m.groups()
+                sets[name].compressions[compression][params].metrics["size"] = size
+
+        size_file.close()
 
 if __name__ == "__main__":
     main(sys.argv[1:])
